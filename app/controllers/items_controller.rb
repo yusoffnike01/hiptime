1baaae10b95d5dacaf82edab0c714c0d9b17e83e
yusoffnike01 <ronaldoyusoff@gmail.com>
2021-03-15 14:48:05 +0800

    update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,10 +4,10 @@ class ItemsController < ApplicationController
         @items=Item.all.order("created_at DESC")
     end
     def new
-        @item =Item.new
+        @item =current_user.items.build
     end
     def create
-        @item=Item.new(item_params)
+        @item=current_user.items.build(item_params)
         if @item.save
             redirect_to root_path
         else
@@ -16,8 +16,22 @@ class ItemsController < ApplicationController
         end
     end
     def show 
-        
     end 
+    def edit
+    end
+    def update
+        if @item.update(item_params)
+            redirect_to item_path(@item)
+        else
+            render 'edit'
+        end 
+    end
+    def destroy 
+        @item.destroy
+        redirect_to root_path
+    end
+
+
     private 
     def item_params
         params.require(:item).permit(:title, :description)
